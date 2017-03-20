8b1c82fb1febdbb3ac6a822d7eea9a3b2f96a5ef
zhonglin xian <xiazl1987@163.com>
2017-03-20 14:14:21 +0800

    first commit pods

---
+++ Example/Pods/Target Support Files/IMMediator/IMMediator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IMMediator : NSObject
+@end
+@implementation PodsDummy_IMMediator
+@end
---
+++ Example/Pods/Target Support Files/IMMediator/IMMediator-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "IMMediator.h"
+
+FOUNDATION_EXPORT double IMMediatorVersionNumber;
+FOUNDATION_EXPORT const unsigned char IMMediatorVersionString[];
+
